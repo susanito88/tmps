2c24cfa76c204f4da760a914e32c30777efd522a
susanito88 <sandumagla@gmail.com>
2025-10-15 13:29:51 +0300

    Added Report.md

--- src/main/java/lab0/Main.java
+++ src/main/java/lab0/Main.java
@@ -2,17 +2,15 @@ package lab0;
 
 public class Main {
     public static void notifyUser(Notification notification, String message){
-        notification.send(message);  // Works for any subclass of Notification
+        notification.send(message);  // any subclass
     }
 
     public static void main(String[] args){
-        // Library example
         Library library = new Library();
         library.addBook(new Book("1984", "George Orwell"));
         library.addBook(new Book("The Hobbit", "J.R.R. Tolkien"));
         library.listBooks();
 
-        // Notifications
         Notification email = new EmailNotification();
         Notification sms = new SMSNotification();
 
